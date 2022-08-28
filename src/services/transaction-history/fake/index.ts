2456ed2bef146fa28d79aa977917446ec63d5c03
Kaio Silveira <silveira.kaio@icloud.com>
2022-08-28 16:58:31 +0100

    move transaction-history-service to its own directory

---
+++ src/services/transaction-history/fake/index.ts
@@ -0,0 +1,25 @@
+import TransactionHistoryService from '..';
+import GlobalConfig from '../../../GlobalConfig';
+
+export default class FakeTransactionHistoryService implements TransactionHistoryService {
+  counter: number;
+  globalConfig: GlobalConfig;
+
+  constructor({ globalConfig }: { globalConfig: GlobalConfig }) {
+    this.counter = 1;
+    this.globalConfig = globalConfig;
+    this.fetchTransactionHistory = this.fetchTransactionHistory.bind(this);
+  }
+
+  async fetchTransactionHistory() {
+    return new Promise((resolve, reject) => {
+      if (this.counter === 4 && !this.globalConfig.CB_OPEN) {
+        this.counter = 1;
+        reject({ msg: 'Service temporarily unavailable' });
+      } else {
+        this.counter++;
+        resolve([{ id: 1, amount: 100, date: new Date() }]);
+      }
+    });
+  }
+}
