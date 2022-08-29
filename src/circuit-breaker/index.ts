489aefd8e218c1fd99e7ab07705086dd6bad2a95
Kaio Silveira <silveira.kaio@icloud.com>
2022-08-29 11:56:03 +0100

    remove comments

--- src/circuit-breaker/index.ts
+++ src/circuit-breaker/index.ts
@@ -1,19 +1,6 @@
-// import { ChildProcess } from 'child_process';
-// import { Request, Response } from 'express';
-
-// import ILogger from '../monitoring/logger';
-// import { LeakyBucketMessage } from '../leaky-bucket/types';
-// import GlobalConfig from '../global-config';
-// import CircuitBreakerState from './state';
-// import CircuitBreakerClosedState from './state/closed';
-// import CircuitBreakerHalfOpenState from './state/half-open';
-// import CircuitBreakerOpenState from './state/open';
-// import { CircuitBreakerStatus } from './status';
-
 export default interface CircuitBreaker {
   open(): void;
   halfOpen(): void;
   close(): void;
   registerFailure(): void;
 }
-
