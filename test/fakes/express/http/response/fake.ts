8f76af4ea68508afe5a8342e0e97e6568b8f8bf1
Kaio Silveira <silveira.kaio@icloud.com>
2022-08-29 17:30:42 +0100

    add fake for express response

---
+++ test/fakes/express/http/response/fake.ts
@@ -0,0 +1,23 @@
+import EventEmitter from 'events';
+
+export default class FakeExpressResponse extends EventEmitter {
+  statusCode: number;
+  body: object;
+  callbacks: Object;
+
+  constructor({ statusCode }: { statusCode: number } = { statusCode: 200 }) {
+    super();
+    this.statusCode = statusCode;
+    this.body = {};
+    this.callbacks = {};
+  }
+
+  status(status: number) {
+    this.statusCode = status;
+    return this;
+  }
+
+  json(obj: any): any {
+    return { body: obj, status: this.statusCode };
+  }
+}
