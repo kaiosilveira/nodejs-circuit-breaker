edab58ee5e01f80f4644508f9b2f78e3b7aac58b
Kaio Silveira <silveira.kaio@icloud.com>
2022-08-29 17:06:50 +0100

    introduce CircuitBreakerOpenStateProps

--- src/circuit-breaker/state/open/index.ts
+++ src/circuit-breaker/state/open/index.ts
@@ -3,8 +3,12 @@ import ExpressCircuitBreaker from '../..';
 import ILogger from '../../../monitoring/logger';
 import { CircuitBreakerStatus } from '../../status';
 
+export type CircuitBreakerOpenStateProps = {
+  circuitBreaker: ExpressCircuitBreaker;
+  logger: ILogger;
+};
 export default class CircuitBreakerOpenState extends CircuitBreakerState {
-  constructor({ circuitBreaker, logger }: { circuitBreaker: ExpressCircuitBreaker; logger: ILogger }) {
+  constructor({ circuitBreaker, logger }: CircuitBreakerOpenStateProps) {
     super({ circuitBreaker, logger, status: CircuitBreakerStatus.OPEN });
     this.circuitBreaker = circuitBreaker;
     this.status = CircuitBreakerStatus.OPEN;
