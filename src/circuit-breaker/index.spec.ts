c5078bb65eaedcfa88e08a4138924ac88614a6e6
Kaio Silveira <silveira.kaio@icloud.com>
2022-08-29 08:40:35 +0100

    add initial implementation for CircuitBreaker

---
+++ src/circuit-breaker/index.spec.ts
@@ -0,0 +1,226 @@
+import { ChildProcess } from 'child_process';
+import EventEmitter from 'events';
+import { Request, Response } from 'express';
+
+import CircuitBreaker, { CircuitBreakerStatus } from '.';
+import GlobalConfig from '../global-config';
+import ILogger from '../logger';
+
+class FakeChildProcess extends ChildProcess {
+  send() {
+    return true;
+  }
+
+  on() {
+    return this;
+  }
+}
+
+class FakeLogger implements ILogger {
+  info(obj: any): void {}
+  error(obj: any): void {}
+}
+
+class FakeGlobalConfig implements GlobalConfig {
+  CB_OPEN: boolean;
+
+  constructor() {
+    this.CB_OPEN = false;
+  }
+
+  isCircuitBreakerOpen(): boolean {
+    return false;
+  }
+
+  setCircuitBreakerOpen(_: boolean): void {}
+}
+
+class FakeExpressResponse extends EventEmitter {
+  statusCode: number;
+  body: object;
+  callbacks: Object;
+
+  constructor({ statusCode }: { statusCode: number } = { statusCode: 200 }) {
+    super();
+    this.statusCode = statusCode;
+    this.body = {};
+    this.callbacks = {};
+  }
+
+  status(status: number) {
+    this.statusCode = status;
+    return this;
+  }
+
+  json(obj: any): any {
+    return { body: obj, status: this.statusCode };
+  }
+}
+
+describe('CircuitBreaker', () => {
+  const threshold = 10;
+  const subscriptionId = 'transaction-history-circuit-breaker';
+  const bucket = new FakeChildProcess();
+  const logger = new FakeLogger();
+  const globalConfig = new FakeGlobalConfig();
+  const next = jest.fn();
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+    next.mockReset();
+  });
+
+  describe('initialization', () => {
+    it('should register itself with the LeakyBucket', () => {
+      const spyOnSend = jest.spyOn(bucket, 'send');
+
+      new CircuitBreaker({
+        bucket,
+        logger,
+        globalConfig,
+        config: { resourceName: 'transaction-history', threshold },
+      });
+
+      expect(spyOnSend).toHaveBeenCalledTimes(1);
+      expect(spyOnSend).toHaveBeenCalledWith({
+        type: 'REGISTER',
+        payload: { subscriptionId, threshold },
+      });
+    });
+
+    it('should register message listeners on the LeakyBucket', () => {
+      const spyOnOn = jest.spyOn(bucket, 'on');
+
+      new CircuitBreaker({
+        bucket,
+        logger,
+        globalConfig,
+        config: { resourceName: 'transaction-history', threshold },
+      });
+
+      expect(spyOnOn).toHaveBeenCalledTimes(1);
+      expect(spyOnOn).toHaveBeenCalledWith('message', expect.anything());
+    });
+  });
+
+  describe('monitor', () => {
+    const threshold = 10;
+    const bucket = new FakeChildProcess();
+    const logger = new FakeLogger();
+    const globalConfig = new FakeGlobalConfig();
+
+    afterEach(() => {
+      jest.restoreAllMocks();
+    });
+
+    it('should return INTERNAL_SERVER_ERROR 500 if state is OPEN', () => {
+      const spyOnLoggerInfo = jest.spyOn(logger, 'info');
+
+      const cb = new CircuitBreaker({
+        bucket,
+        logger,
+        globalConfig,
+        config: { resourceName: 'transaction-history', threshold },
+      });
+
+      cb.setState(CircuitBreakerStatus.OPEN);
+
+      const req = {} as Request;
+      const res = new FakeExpressResponse() as unknown as Response;
+      const next = () => {};
+
+      const result = cb.monitor(req, res, next) as Response;
+
+      expect(result.status).toEqual(500);
+      expect(spyOnLoggerInfo).toHaveBeenCalledTimes(1);
+      expect(spyOnLoggerInfo).toHaveBeenCalledWith({
+        msg: 'Call refused from circuit breaker',
+        status: CircuitBreakerStatus.OPEN,
+      });
+    });
+
+    it('should register success 200 OK response when the state is CLOSED', () => {
+      const spyOnLoggerInfo = jest.spyOn(logger, 'info');
+
+      const cb = new CircuitBreaker({
+        bucket,
+        logger,
+        globalConfig,
+        config: { resourceName: 'transaction-history', threshold },
+      });
+
+      cb.setState(CircuitBreakerStatus.CLOSED);
+
+      const req = {} as Request;
+      const res = new FakeExpressResponse() as unknown as Response;
+
+      cb.monitor(req, res, next) as Response;
+      res.emit('finish');
+
+      expect(next).toHaveBeenCalledTimes(1);
+      expect(spyOnLoggerInfo).toHaveBeenCalledTimes(1);
+      expect(spyOnLoggerInfo).toHaveBeenCalledWith({
+        msg: 'Successful response',
+        status: CircuitBreakerStatus.CLOSED,
+      });
+    });
+
+    it('should close the circuit again if response has status 200 OK and circuit is at HALF_OPEN', () => {
+      const spyOnLoggerInfo = jest.spyOn(logger, 'info');
+
+      const cb = new CircuitBreaker({
+        bucket,
+        logger,
+        globalConfig,
+        config: { resourceName: 'transaction-history', threshold },
+      });
+
+      cb.halfOpen();
+
+      const req = {} as Request;
+      const res = new FakeExpressResponse() as unknown as Response;
+
+      cb.monitor(req, res, next) as Response;
+      res.emit('finish');
+
+      expect(next).toHaveBeenCalledTimes(1);
+      expect(spyOnLoggerInfo).toHaveBeenCalledTimes(1);
+      expect(spyOnLoggerInfo).toHaveBeenCalledWith({
+        msg: 'Successful response while in a HALF_OPEN state. Circuit is now closed.',
+        status: CircuitBreakerStatus.CLOSED,
+      });
+    });
+
+    it('should open the circuit again if response 500 INTERNAL SERVER ERROR and circuit is at HALF_OPEN', () => {
+      const spyOnLoggerInfo = jest.spyOn(logger, 'info');
+      const spyOnBucketSend = jest.spyOn(bucket, 'send');
+
+      const cb = new CircuitBreaker({
+        bucket,
+        logger,
+        globalConfig,
+        config: { resourceName: 'transaction-history', threshold },
+      });
+
+      cb.halfOpen();
+
+      const req = {} as Request;
+      const res = new FakeExpressResponse({ statusCode: 500 }) as unknown as Response;
+      const next = () => {};
+
+      cb.monitor(req, res, next) as Response;
+      res.emit('finish');
+
+      expect(spyOnLoggerInfo).toHaveBeenCalledTimes(1);
+      expect(spyOnLoggerInfo).toHaveBeenCalledWith({
+        msg: 'Failure response while in a HALF_OPEN state. Opening circuit.',
+        status: CircuitBreakerStatus.OPEN,
+      });
+
+      expect(spyOnBucketSend).toHaveBeenCalledWith({
+        type: 'NEW_FAILURE',
+        payload: { subscriptionId },
+      });
+    });
+  });
+});
--- src/circuit-breaker/index.ts
+++ src/circuit-breaker/index.ts
@@ -11,86 +11,126 @@ export enum CircuitBreakerStatus {
   HALF_OPEN = 'half_open',
 }
 
+interface CircuitBreakerState {
+  handleOkResponse(): void;
+  handleInternalServerErrorResponse(): void;
+}
+
+class CircuitBreakerClosedState implements CircuitBreakerState {
+  logger: ILogger;
+  circuitBreaker: CircuitBreaker;
+
+  constructor({ circuitBreaker, logger }: { circuitBreaker: CircuitBreaker; logger: ILogger }) {
+    this.logger = logger;
+    this.circuitBreaker = circuitBreaker;
+  }
+
+  handleOkResponse(): void {
+    this.logger.info({ msg: 'Successful response', status: this.circuitBreaker._state });
+  }
+
+  handleInternalServerErrorResponse(): void {
+    this.circuitBreaker.registerFailure();
+  }
+}
+
+class CircuitBreakerHalfOpenState implements CircuitBreakerState {
+  circuitBreaker: CircuitBreaker;
+  logger: ILogger;
+
+  constructor({ circuitBreaker, logger }: { circuitBreaker: CircuitBreaker; logger: ILogger }) {
+    this.circuitBreaker = circuitBreaker;
+    this.logger = logger;
+  }
+
+  handleOkResponse(): void {
+    this.circuitBreaker.close();
+    this.logger.info({
+      msg: 'Successful response while in a HALF_OPEN state. Circuit is now closed.',
+      status: this.circuitBreaker._state,
+    });
+  }
+
+  handleInternalServerErrorResponse(): void {
+    this.circuitBreaker.open();
+    this.logger.info({
+      msg: 'Failure response while in a HALF_OPEN state. Opening circuit.',
+      status: this.circuitBreaker._state,
+    });
+
+    this.circuitBreaker.registerFailure();
+  }
+}
+
+class CircuitBreakerOpenState implements CircuitBreakerState {
+  circuitBreaker: CircuitBreaker;
+  constructor({ circuitBreaker }: { circuitBreaker: CircuitBreaker }) {
+    this.circuitBreaker = circuitBreaker;
+  }
+
+  handleOkResponse(): void {}
+
+  handleInternalServerErrorResponse(): void {
+    this.circuitBreaker.registerFailure();
+  }
+}
+
+export type CircuitBreakerConfig = { resourceName: string; threshold: number };
 export default class CircuitBreaker {
-  subscriptionId: string = 'abc';
+  subscriptionId: string;
   logger: ILogger;
   bucket: ChildProcess;
-  status: CircuitBreakerStatus;
+  _state: CircuitBreakerStatus;
+  config: CircuitBreakerConfig;
   globalConfig: GlobalConfig;
+  state: CircuitBreakerState;
 
   constructor({
     bucket,
     logger,
+    config,
     globalConfig,
   }: {
     bucket: ChildProcess;
     logger: ILogger;
+    config: CircuitBreakerConfig;
     globalConfig: GlobalConfig;
   }) {
+    this.config = config;
     this.bucket = bucket;
     this.logger = logger;
     this.globalConfig = globalConfig;
-    this.status = CircuitBreakerStatus.CLOSED;
+    this.subscriptionId = `${this.config.resourceName}-circuit-breaker`;
+    this._state = CircuitBreakerStatus.CLOSED;
+    this.state = new CircuitBreakerClosedState({ circuitBreaker: this, logger: this.logger });
 
     this.monitor = this.monitor.bind(this);
     this.close = this.close.bind(this);
+    this.registerFailure = this.registerFailure.bind(this);
 
-    this.bucket.send({
-      type: 'REGISTER',
-      payload: { subscriptionId: this.subscriptionId, threshold: 2 },
-    });
+    this._handleInternalServerErrorResponse = this._handleInternalServerErrorResponse.bind(this);
+    this._setupLeakyBucket = this._setupLeakyBucket.bind(this);
+    this._handleBucketMessage = this._handleBucketMessage.bind(this);
 
-    this.bucket.on('message', (msg: LeakyBucketMessage) => {
-      switch (msg.type) {
-        case 'THRESHOLD_VIOLATION':
-          this.logger.info({ msg: 'Threshold violated. Opening circuit.' });
-          this.open();
-          break;
-        case 'THRESHOLD_RESTORED':
-          this.halfOpen();
-          this.logger.info({
-            msg: 'Threshold restored. Moving circuit to half-open.',
-            status: this.status,
-          });
-          break;
-        default:
-          break;
-      }
-    });
+    this._setupLeakyBucket();
   }
 
   monitor(_: Request, res: Response, next: Function): void | Response {
-    if (this.status === CircuitBreakerStatus.OPEN) {
-      this.logger.info({ msg: 'Call refused from circuit breaker', status: this.status });
+    if (this._state === CircuitBreakerStatus.OPEN) {
+      this.logger.info({ msg: 'Call refused from circuit breaker', status: this._state });
       return res.status(500).json({ msg: 'Call refused from circuit breaker' });
     }
 
     res.on('finish', () => {
-      if (this.status === CircuitBreakerStatus.HALF_OPEN) {
-        if (res.statusCode === 200) {
-          this.logger.info({
-            msg: 'Successful response while in a HALF_OPEN state. Closing circuit.',
-            status: this.status,
-          });
-          this.close();
-        } else {
-          this.open();
-          this.logger.info({
-            msg: 'Failure response while in a HALF_OPEN state. Opening circuit.',
-            status: this.status,
-          });
-        }
-      }
-
-      if (res.statusCode === 500) {
-        this.bucket.send({
-          type: 'NEW_FAILURE',
-          payload: { subscriptionId: this.subscriptionId },
-        } as LeakyBucketMessage);
-      }
-
-      if (res.statusCode === 200) {
-        this.logger.info({ msg: 'Successful response', status: this.status });
+      switch (res.statusCode) {
+        case 200:
+          this.state.handleOkResponse();
+          break;
+        case 500:
+          this._handleInternalServerErrorResponse();
+          break;
+        default:
+          break;
       }
     });
 
@@ -99,16 +139,76 @@ export default class CircuitBreaker {
 
   close(): void {
     this.globalConfig.setCircuitBreakerOpen(false);
-    this.status = CircuitBreakerStatus.CLOSED;
+    this.state = new CircuitBreakerClosedState({ circuitBreaker: this, logger: this.logger });
+    this._state = CircuitBreakerStatus.CLOSED;
   }
 
   open(): void {
     this.globalConfig.setCircuitBreakerOpen(true);
-    this.status = CircuitBreakerStatus.OPEN;
+    this.state = new CircuitBreakerOpenState({ circuitBreaker: this });
+    this._state = CircuitBreakerStatus.OPEN;
   }
 
   halfOpen(): void {
     this.globalConfig.setCircuitBreakerOpen(false);
-    this.status = CircuitBreakerStatus.HALF_OPEN;
+    this.state = new CircuitBreakerHalfOpenState({ circuitBreaker: this, logger: this.logger });
+    this._state = CircuitBreakerStatus.HALF_OPEN;
+  }
+
+  setState(status: CircuitBreakerStatus): void {
+    this._state = status;
+  }
+
+  private _handleInternalServerErrorResponse() {
+    switch (this._state) {
+      case CircuitBreakerStatus.HALF_OPEN:
+        this.open();
+        this.logger.info({
+          msg: 'Failure response while in a HALF_OPEN state. Opening circuit.',
+          status: this._state,
+        });
+        break;
+      default:
+        break;
+    }
+
+    this.registerFailure();
+  }
+
+  registerFailure() {
+    this.bucket.send({
+      type: 'NEW_FAILURE',
+      payload: { subscriptionId: this.subscriptionId },
+    } as LeakyBucketMessage);
+  }
+
+  private _handleBucketMessage(msg: LeakyBucketMessage) {
+    switch (msg.type) {
+      case 'THRESHOLD_VIOLATION':
+        this.logger.info({ msg: 'Threshold violated. Opening circuit.' });
+        this.open();
+        break;
+      case 'THRESHOLD_RESTORED':
+        this.halfOpen();
+        this.logger.info({
+          msg: 'Threshold restored. Moving circuit to half-open.',
+          status: this._state,
+        });
+        break;
+      default:
+        break;
+    }
+  }
+
+  private _setupLeakyBucket() {
+    this.bucket.send({
+      type: 'REGISTER',
+      payload: {
+        subscriptionId: this.subscriptionId,
+        threshold: this.config.threshold,
+      },
+    });
+
+    this.bucket.on('message', this._handleBucketMessage);
   }
 }
