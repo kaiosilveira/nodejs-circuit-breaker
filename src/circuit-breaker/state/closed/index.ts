c93ac7e8ef2df4bfce38dd17f641c7b30d89eb8b
Kaio Silveira <silveira.kaio@icloud.com>
2022-08-29 10:56:38 +0100

    move CircuitBreakerClosedState to its own file

---
+++ src/circuit-breaker/state/closed/index.ts
@@ -0,0 +1,18 @@
+import CircuitBreakerState from '..';
+import CircuitBreaker from '../..';
+import ILogger from '../../../monitoring/logger';
+import { CircuitBreakerStatus } from '../../status';
+
+export default class CircuitBreakerClosedState extends CircuitBreakerState {
+  constructor({ circuitBreaker, logger }: { circuitBreaker: CircuitBreaker; logger: ILogger }) {
+    super({ status: CircuitBreakerStatus.CLOSED, circuitBreaker, logger });
+  }
+
+  handleOkResponse(): void {
+    this.logger.info({ msg: 'Successful response', status: this.circuitBreaker._state });
+  }
+
+  handleInternalServerErrorResponse(): void {
+    this.circuitBreaker.registerFailure();
+  }
+}
