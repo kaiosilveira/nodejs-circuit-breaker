b2ee31472697e4bee17f40af164b6952edd6f26d
Kaio Silveira <silveira.kaio@icloud.com>
2022-09-01 10:06:32 +0100

    use LeakyBucketMessages at LeakyBucekt's process definition

--- src/leaky-bucket/process-definition.ts
+++ src/leaky-bucket/process-definition.ts
@@ -1,4 +1,5 @@
 import LeakyBucket from '.';
+import { LeakyBucketMessages } from './messages';
 import { LeakyBucketMessage } from './types';
 
 const bucket = new LeakyBucket();
@@ -6,16 +7,19 @@ const bucket = new LeakyBucket();
 process.on('message', (msg: LeakyBucketMessage) => {
   const { subscriptionId } = msg.payload;
   switch (msg.type) {
-    case 'REGISTER':
+    case LeakyBucketMessages.REGISTER:
       bucket.subscribe({ subscriptionId, threshold: msg.payload.threshold });
       break;
-    case 'NEW_FAILURE':
+    case LeakyBucketMessages.NEW_FAILURE:
       bucket.increment({ subscriptionId });
       if (bucket.isAboveThreshold({ subscriptionId })) {
-        process.send?.({ type: 'THRESHOLD_VIOLATION', payload: { subscriptionId } });
+        process.send?.({
+          type: LeakyBucketMessages.THRESHOLD_VIOLATION,
+          payload: { subscriptionId },
+        });
       }
       break;
-    case 'RESET':
+    case LeakyBucketMessages.RESET:
       bucket.resetCountFor({ subscriptionId });
       break;
     default:
@@ -28,7 +32,7 @@ setInterval(() => {
     const currentCount = bucket.fetchCountFor({ subscriptionId });
     const threshold = bucket.fetchThresholdFor({ subscriptionId });
     if (currentCount - threshold === 1) {
-      process.send?.({ type: 'THRESHOLD_RESTORED', subscriptionId });
+      process.send?.({ type: LeakyBucketMessages.THRESHOLD_RESTORED, subscriptionId });
     }
     bucket.decrement({ subscriptionId });
   });
