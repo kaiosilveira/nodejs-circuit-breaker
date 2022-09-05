2cc435eebd026f35b83381b6bf43e92dad7c1cc6
Kaio Silveira <silveira.kaio@icloud.com>
2022-09-05 19:30:13 +0200

    add setCircuitBreakerState to ApplicationState

--- src/app/infra/application-state/index.ts
+++ src/app/infra/application-state/index.ts
@@ -4,4 +4,11 @@ import { CircuitBreakerStatus } from '../../stability/circuit-breaker/status';
 export default interface ApplicationState {
   fetchCircuitBreakerState(circuitBreakerId: string): CircuitBreakerStatus;
   registerCircuitBreaker(circuitBreaker: CircuitBreaker): void;
+  setCircuitBreakerState({
+    circuitBreakerId,
+    state,
+  }: {
+    circuitBreakerId: string;
+    state: CircuitBreakerStatus;
+  }): void;
 }
