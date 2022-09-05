e50c6756fd8b3083a410ac0d2795aa5b22cd57bd
Kaio Silveira <silveira.kaio@icloud.com>
2022-09-05 19:28:58 +0200

    update FakeCircuitBreaker

--- src/app/stability/circuit-breaker/fake/index.ts
+++ src/app/stability/circuit-breaker/fake/index.ts
@@ -3,16 +3,34 @@ import CircuitBreaker from '..';
 import { CircuitBreakerStatus } from '../status';
 
 export default class FakeCircuitBreaker extends EventEmitter implements CircuitBreaker {
+  _id: string;
+  _status: CircuitBreakerStatus;
+
+  constructor({ id } = { id: 'abc' }) {
+    super();
+    this._id = id;
+    this._status = CircuitBreakerStatus.CLOSED;
+  }
+
   getStatus(): CircuitBreakerStatus {
-    return CircuitBreakerStatus.CLOSED;
+    return this._status;
   }
 
   getIdentifier(): string {
-    return 'abc';
+    return this._id;
   }
 
-  open(): void {}
-  halfOpen(): void {}
-  close(): void {}
+  open(): void {
+    this._status = CircuitBreakerStatus.OPEN;
+  }
+
+  halfOpen(): void {
+    this._status = CircuitBreakerStatus.HALF_OPEN;
+  }
+
+  close(): void {
+    this._status = CircuitBreakerStatus.CLOSED;
+  }
+
   registerFailure(): void {}
 }
