6599ad6130fcf1ef60cbb62558a6498665d52e5a
Kaio Silveira <silveira.kaio@icloud.com>
2022-09-13 19:21:50 +0100

    add IncomingRequestMiddleware

---
+++ src/interface-adapters/express/middlewares/incoming-request/index.test.ts
@@ -0,0 +1,22 @@
+import IncomingRequestMiddleware from '.';
+import FakeLogger from '../../../../app/infra/logger/fake';
+import FakeExpressRequest from '../../../../../test/fakes/express/http/request';
+import FakeExpressResponse from '../../../../../test/fakes/express/http/response';
+import { Request, Response } from 'express';
+
+describe('IncomingRequestMiddleware', () => {
+  it('should create a context for the request with an actionUUID', () => {
+    const generatedUUID = 'abc';
+    const generateUUID = jest.fn().mockReturnValue(generatedUUID);
+    const next = jest.fn();
+
+    const req = new FakeExpressRequest({ body: {} }) as unknown as Request;
+    const res = new FakeExpressResponse() as unknown as Response;
+
+    new IncomingRequestMiddleware({ logger: new FakeLogger(), generateUUID }).hook(req, res, next);
+
+    expect(req.context?.actionUUID).toEqual(generatedUUID);
+    expect(generateUUID).toHaveBeenCalled();
+    expect(next).toHaveBeenCalled();
+  });
+});
---
+++ src/interface-adapters/express/middlewares/incoming-request/index.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from 'express';
+import ILogger from '../../../../app/infra/logger';
+
+class IncomingRequestMiddleware {
+  _logger: ILogger;
+  _generateUUID: Function;
+
+  constructor({ logger, generateUUID }: { logger: ILogger; generateUUID: Function }) {
+    this._logger = logger;
+    this._generateUUID = generateUUID;
+
+    this.hook = this.hook.bind(this);
+  }
+
+  hook(req: Request, _: Response, next: Function): void {
+    const context = {
+      actionUUID: this._generateUUID(),
+      reqStartedAt: +new Date(),
+      method: req.method,
+      url: req.url,
+    };
+
+    req.context = context;
+    next();
+  }
+}
+
+export default IncomingRequestMiddleware;
