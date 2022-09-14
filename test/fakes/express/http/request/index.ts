2f075235bb49734c2d92ab4fb3784294258b9557
Kaio Silveira <silveira.kaio@icloud.com>
2022-09-14 17:09:19 +0100

    add headers config to FakeExpressRequest

--- test/fakes/express/http/request/index.ts
+++ test/fakes/express/http/request/index.ts
@@ -1,7 +1,9 @@
 export default class FakeExpressRequest {
   body: Object;
+  headers: Object;
 
-  constructor({ body }) {
+  constructor({ body, headers }: { body?: any; headers?: any } = { body: {}, headers: {} }) {
     this.body = body;
+    this.headers = headers;
   }
 }
